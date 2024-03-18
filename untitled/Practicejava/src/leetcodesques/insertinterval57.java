8158c1b5b622ff910327a90542d2b3d2ce198717
rishitkamboj <rishit242@gmail.com>
2024-03-18 05:57:15 +0530

    DSA files

---
+++ untitled/Practicejava/src/leetcodesques/insertinterval57.java
@@ -0,0 +1,31 @@
+package leetcodesques;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class insertinterval57 {
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> r = new ArrayList<>();
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            r.add(intervals[i]);
+            i++;
+        }
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        r.add(newInterval);
+        while (i < intervals.length) {
+            r.add(intervals[i]);
+            i++;
+        }
+
+        return r.toArray(new int[r.size()][]);
+
+
+    }
+}
