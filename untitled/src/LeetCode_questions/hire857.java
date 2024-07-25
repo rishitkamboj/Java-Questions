3f141eac795e7464fe93de2933225e0d6fc9d92e
rishit <rishit242@gmail.com>
2024-07-25 18:32:00 +0530

    lab questions ik youre stalking my repo

--- untitled/src/LeetCode_questions/hire857.java
+++ untitled/src/LeetCode_questions/hire857.java
@@ -1,28 +1,28 @@
-package LeetCode_questions;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-
-public class hire857 {
-    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        int n=wage.length;
-        double minCost=Double.MAX_VALUE;
-
-        for(int caption=0;caption<n;caption++){
-            double captionratio=(double)(wage[caption]/quality[caption]);
-            ArrayList<Double> expected=new ArrayList<>();
-            for(int worker=0;worker<n;worker++){
-                double offer=quality[worker]*captionratio;
-                if(offer>=wage[worker]){
-                    expected.add(offer);
-                }
-            }
-            if(expected.size()<k){
-                continue;
-            }
-           
-        }
-
-    }
-}
+//package LeetCode_questions;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.PriorityQueue;
+//
+//public class hire857 {
+//    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+//        int n=wage.length;
+//        double minCost=Double.MAX_VALUE;
+//
+//        for(int caption=0;caption<n;caption++){
+//            double captionratio=(double)(wage[caption]/quality[caption]);
+//            ArrayList<Double> expected=new ArrayList<>();
+//            for(int worker=0;worker<n;worker++){
+//                double offer=quality[worker]*captionratio;
+//                if(offer>=wage[worker]){
+//                    expected.add(offer);
+//                }
+//            }
+//            if(expected.size()<k){
+//                continue;
+//            }
+//
+//        }
+//
+//    }
+//}
